f6c1d6240014595c7366715dd8c9b3fd483aaf08
Arctic Fox <tyskanhik@mail.ru>
2026-03-19 16:35:27 +0300

    update routers and config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  },
+  {
+    path: 'books',
+    loadComponent: () => import('./features/book-list/book-list.component')
+      .then(m => m.BookListComponent),
+    title: 'Список книг'
+  },
+  {
+    path: 'books/create',
+    loadComponent: () => import('./features/book-form/book-form.component')
+      .then(m => m.BookFormComponent),
+    title: 'Добавить книгу'
+  }
+];
